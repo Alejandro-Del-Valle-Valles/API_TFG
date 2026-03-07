6ec9e9aff15a788dd4af3fbb44ab48df49aa6813
Alejandro del Valle Vallés <163209907+Alejandro-Del-Valle-Valles@users.noreply.github.com>
2026-03-07 23:28:39 +0100

    Refactor SalaId to use cineId instead of cine

--- src/main/java/com/tfg/API_TFG/core/entity/id/SalaId.java
+++ src/main/java/com/tfg/API_TFG/core/entity/id/SalaId.java
@@ -6,27 +6,38 @@ import java.util.Objects;
 /**
  * Clase que serializa la clave de Sala al ser esta una entidad debil
  */
+@Embeddable
 public class SalaId implements Serializable {
 
-    private Long cine;
+    @Column(name = "cine_id")
+    private Long cineId;
+
+    @Column(name = "numero_sala")
     private Integer numeroSala;
 
-    public SalaId() { }
+    public SalaId() {}
 
-    public SalaId(Long cine, Integer numeroSala) {
-        this.cine = cine;
+    public SalaId(Long cineId, Integer numeroSala) {
+        this.cineId = cineId;
         this.numeroSala = numeroSala;
     }
+
+    public Long getCineId() { return cineId; }
+    public void setCineId(Long cineId) { this.cineId = cineId; }
+
+    public Integer getNumeroSala() { return numeroSala; }
+    public void setNumeroSala(Integer numeroSala) { this.numeroSala = numeroSala; }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         SalaId salaId = (SalaId) o;
-        return Objects.equals(cine, salaId.cine) && Objects.equals(numeroSala, salaId.numeroSala);
+        return Objects.equals(cineId, salaId.cineId) &&
+               Objects.equals(numeroSala, salaId.numeroSala);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(cine, numeroSala);
+        return Objects.hash(cineId, numeroSala);
     }
 }
