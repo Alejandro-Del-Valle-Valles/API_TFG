53cf9065aa459383f53bbc4291f435e9081dac69
Alejandro <alejandrodelvalle06@outlook.es>
2026-04-11 14:34:52 +0200

    Soluciono un problema con el DTO de entrada

--- src/main/java/com/tfg/API_TFG/core/dto/EntradaDTO.java
+++ src/main/java/com/tfg/API_TFG/core/dto/EntradaDTO.java
@@ -2,6 +2,7 @@ package com.tfg.API_TFG.core.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
@@ -20,12 +21,16 @@ public record EntradaDTO(
                 description = "Número de la fila",
                 example = "5"
         )
+        @NotNull(message = "El numero de la fila no puede ser nulo.")
+        @Positive(message = "El numero de la fila no puede ser inferior a 1.")
         Integer numFila,
 
         @Schema(
                 description = "Número de la butaca",
                 example = "2"
         )
+        @NotNull(message = "El numero de la butaca no puede ser nulo.")
+        @Positive(message = "El numero de butaca no puede ser inferior a 1.")
         Integer numButaca,
 
         @Schema(
